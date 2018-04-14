bdf5b29b311021ccf9c163f3f560e1b5d821f0c3
Edwin Zhao <edwin.zhao@outlook.com>
2018-04-14 14:52:10 -0400

    Added two scenes.

--- AudiologyNLP.java
+++ AudiologyNLP.java
@@ -25,67 +25,74 @@ import javafx.stage.Stage;
 public class AudiologyNLP extends Application {
     private static final int SCREENWIDTH = 900;
     private static final int SCREENHEIGHT = 600;
-
-
+    private LoginScene login;
+    private MainScene main;
+    private Stage stage;
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("AudiologyNLP");
+        stage = primaryStage;
+        Scene scene = startApp();
+        stage.setTitle("AudiologyNLP");
 
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(15));
+        // login = new Scene(grid, SCREENWIDTH, SCREENHEIGHT);
+        stage.setScene(scene);
+        stage.show();
 
-        // MenuBar menuBar = new MenuBar();
-        // menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
-        // grid.setTop(menuBar);
+        stage.setHeight(SCREENHEIGHT);
+        stage.setWidth(SCREENWIDTH);
+    }
 
-        Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Verdana", FontWeight.NORMAL, 25));
-        grid.add(scenetitle, 0, 0, 2, 1);
 
-        Label username = new Label("User Name:");
-        grid.add(username, 0, 1);
-
-        TextField loginField = new TextField();
-        grid.add(loginField, 1, 1);
-
-        Label password = new Label("Password:");
-        grid.add(password, 0, 2);
-
-        PasswordField passField = new PasswordField();
-        grid.add(passField, 1, 2);
-
-        Button loginBtn = new Button("Sign in");
-        HBox hbLogin = new HBox(10);
-        hbLogin.setAlignment(Pos.BOTTOM_RIGHT);
-        hbLogin.getChildren().add(loginBtn);
-        grid.add(hbLogin, 1, 4);
-
-        final Text actionText = new Text();
-        grid.add(actionText, 1, 6);
+    public Scene startApp() {
+        login = new LoginScene();
+        Button loginBtn = login.getLoginBtn();
 
         loginBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                if (passField.getText().equals("asdf")) {
-                    actionText.setFill(Color.FIREBRICK);
-                    actionText.setText("Incorrect Password");
+                if (login.getPassField().getText().equals("asdf")) {
+                    main = new MainScene();
+                    stage.setScene(new Scene(main));
                 } else {
+                    login.getActionText().setFill(Color.FIREBRICK);
+                    login.getActionText().setText("Incorrect Password");
                 }
             }
         });
 
-        Scene scene = new Scene(grid, SCREENWIDTH, SCREENHEIGHT);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        return new Scene(login);
     }
+
+//     public VBox addVBox(); {
+//         VBox vbox = new VBox();
+//         vbox.setPadding(new Insets(10));
+//         vbox.setSpacing(8);
+
+//         Text title = new Text("Data");
+//         title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+//         vbox.getChildren().add(title);
+
+//         Hyperlink options[] = new Hyperlink[] {
+//             new Hyperlink("Sales"),
+//             new Hyperlink("Marketing"),
+//             new Hyperlink("Distribution"),
+//             new Hyperlink("Costs")};
+
+//         for (int i=0; i<4; i++) {
+//             VBox.setMargin(options[i], new Insets(0, 0, 0, 8));
+//             vbox.getChildren().add(options[i]);
+//         }
+
+//     return vbox;
+// }
+
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
 
 }
