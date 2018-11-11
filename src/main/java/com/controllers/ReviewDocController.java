0596bbcc7d4abb301c3d6f614a424f2ffa6aeee3
Joonhyuk Chun <joon1032@gmail.com>
2018-11-11 15:49:49 -0500

    Lines starting

--- src/main/java/com/controllers/ReviewDocController.java
+++ src/main/java/com/controllers/ReviewDocController.java
@@ -15,7 +15,19 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.scene.control.TextArea;
+
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ReviewDocController implements Initializable {
     private static App app;
@@ -27,9 +39,9 @@ public class ReviewDocController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        app = App.getInstance();
-        user = app.getUser();
-        file = Paths.get("out.txt");
+//        app = App.getInstance();
+//        user = app.getUser();
+//        file = Paths.get("out.txt");
 //        try {
 //
 ////            Scanner scan = new Scanner(file).useDelimiter("\\s+");
@@ -41,6 +53,95 @@ public class ReviewDocController implements Initializable {
 //        } catch (IOException e) {
 //            System.out.println(e);
 //        }
+        //Loading the tokenizer model
+        try {
+            InputStream inputStreamTokenizer = new
+                    FileInputStream("./models/en-token.bin");
+            TokenizerModel tokenModel = new TokenizerModel(inputStreamTokenizer);
+
+            //Instantiating the TokenizerME class
+            TokenizerME tokenizer = new TokenizerME(tokenModel);
+
+            //read the input text as a single string
+
+            FileReader in = new FileReader("./Report.txt");
+            BufferedReader br = new BufferedReader(in);
+
+            String line = br.readLine();
+            String sentence = line;
+            while (line != null) {
+                sentence += line;
+                line = br.readLine();
+            }
+
+            //Tokenizing the sentence in to a string array
+            String tokens[] = tokenizer.tokenize(sentence);
+
+            //Loading the name finder model
+            InputStream inputStreamNameFinder = new FileInputStream("./models/en"
+                    + "-ner-person.bin");
+            TokenNameFinderModel name_model = new TokenNameFinderModel(inputStreamNameFinder);
+
+            NameFinderME nameFinder = new NameFinderME(name_model);
+            Span nameSpans[] = nameFinder.find(tokens);
+
+            //Printing the names and their spans in a sentence
+            System.out.print("NAMES: ");
+            for (Span s : nameSpans) {
+                //print out the name as well as its location
+                System.out.println(s.toString() + "  " + tokens[s.getStart()]);
+
+                //only print out name
+                //System.out.println(tokens[s.getStart()]);
+            }
+
+            //loading the date finder model
+            InputStream inputStreamNameFinder2 = new FileInputStream("./models/en"
+                    + "-ner-date.bin");
+            TokenNameFinderModel date_model = new TokenNameFinderModel(inputStreamNameFinder2);
+
+            NameFinderME dateFinder = new NameFinderME(date_model);
+            Span dateSpans[] = dateFinder.find(tokens);
+            System.out.println();
+            //Printing the names and their spans in a sentence
+            System.out.print("DATES: ");
+            for (Span s : dateSpans) {
+                System.out.println(s.toString() + "  " + tokens[s.getStart()]);
+            }
+
+            //loading the location finder model
+            InputStream inputStreamNameFinder3 = new FileInputStream("./models/en"
+                    + "-ner-location.bin");
+            TokenNameFinderModel location_model = new TokenNameFinderModel(inputStreamNameFinder3);
+
+            NameFinderME locationFinder = new NameFinderME(location_model);
+            Span locationSpans[] = locationFinder.find(tokens);
+
+            System.out.println();
+            //Printing the names and their spans in a sentence
+            System.out.print("LOCATIONS: ");
+            for (Span s : locationSpans) {
+                System.out.println(s.toString() + "  " + tokens[s.getStart()]);
+            }
+
+            //loading the organization finder model
+            InputStream inputStreamNameFinder4 = new FileInputStream("./models/en"
+                    + "-ner-organization.bin");
+            TokenNameFinderModel org_model = new TokenNameFinderModel(inputStreamNameFinder4);
+
+            NameFinderME orgFinder = new NameFinderME(org_model);
+            Span orgSpans[] = orgFinder.find(tokens);
+
+            System.out.println();
+            //Printing the names and their spans in a sentence
+            System.out.print("Organizations: ");
+            for (Span s : orgSpans) {
+                System.out.println(s.toString() + "  " + tokens[s.getStart()]);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
     }
 
 
